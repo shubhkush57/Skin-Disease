3369e7e98a9533709e1eea7265d574432054a2aa
Shubahm <shubham.20205157@mnnit.ac.in>
2024-05-20 11:24:21 +0530

    About Page Updated

--- client/src/components/layout/About.js
+++ client/src/components/layout/About.js
@@ -51,7 +51,31 @@ function About() {
 
   return (
     <div className="container mt-8 flex flex-col items-center">
-  <div>
+
+
+
+<div>
+        <h1 className="text-3xl font-bold mb-4 text-center">Information About {renderResult()}</h1>
+      </div>
+      <div className="mt-4 w-full max-w-md">
+        <select
+          className="form-select w-full py-3 px-4 bg-white border border-gray-300 rounded-md shadow-md focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500 transition duration-200"
+          value={car}
+          onChange={handleOnChange}
+        >
+          <option value="Select Disease" disabled>Select Disease Type</option>
+          <option value="Acne">Acne</option>
+          <option value="Actinic">Actinic</option>
+          <option value="Basal">Basal</option>
+          <option value="Benign">Benign</option>
+          <option value="Atopic">Atopic</option>
+          <option value="Melanoma">Melanoma</option>
+        </select>
+      </div>
+
+
+      
+  {/* <div>
     <h1 className="text-3xl font-bold mb-4">Information About {renderResult()}</h1>
   </div>
   <div className="mt-4">
@@ -66,7 +90,7 @@ function About() {
         <option value="Melanoma">Melanoma</option>
       </select>
     </div>
-  </div>
+  </div> */}
   <div className="mt-8">
     {MelanomaContentVisible && <Melanoma />}
     {AcneContentVisible && <Acne />}
